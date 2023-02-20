1c2fdd8b9e2b3f186eaabeb9fd5dc9ac4abcbe48
Rizwan Syed <78642744+mr-rizwan-syed@users.noreply.github.com>
2023-02-20 20:12:28 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 apt install python3 -y
 apt install python3-pip -y
 apt install git -y
-apt install nmap
+apt install nmap -y
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install github.com/lc/gau/v2/cmd/gau@latest
 go install github.com/tomnomnom/waybackurls@latest
