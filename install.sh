dd00093250faee7794764e8371b3a3638c46e561
Rizwan Syed <78642744+mr-rizwan-syed@users.noreply.github.com>
2023-10-11 13:51:32 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -98,7 +98,7 @@ dependency_installer(){
     if ! check_exist httpx; then
         echo "${YELLOW}[*] Installing httpx ${NC}"
         rm "$(which httpx)" 2>/dev/null
-        go install -v github.com/projectdiscovery/httpx/cmd/httpx@v1.3.0 2>/dev/null | pv -p -t -e -N "Installing Tool: httpx" >/dev/null
+        go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest 2>/dev/null | pv -p -t -e -N "Installing Tool: httpx" >/dev/null
     fi
     if ! check_exist tlsx; then
         echo "${YELLOW}[*] Installing TLSx ${NC}"
@@ -157,14 +157,14 @@ dependency_installer(){
         echo "${YELLOW}[*] Installing naabu ${NC}"
         # go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest 2>/dev/null | pv -p -t -e -N "Installing Tool: Naabu" &>/dev/null
         # If Naabu is not getting installed by below command, download the compiled binary from official naabu github release page.
-        # go install github.com/projectdiscovery/naabu/v2/cmd/naabu@latest > /dev/null 2>&1 | pv -p -t -e -N "Installing Tool: naabu"
-        rm "$(which naabu)" 2>/dev/null
-        wget https://github.com/projectdiscovery/naabu/releases/download/v2.1.0/naabu_2.1.0_linux_amd64.zip -P ./MISC/naabu &>/dev/null
+        go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest 2>/dev/null | pv -p -t -e -N "Installing Tool: naabu" >/dev/null
+        #rm "$(which naabu)" 2>/dev/null
+        #wget https://github.com/projectdiscovery/naabu/releases/download/v2.1.0/naabu_2.1.0_linux_amd64.zip -P ./MISC/naabu &>/dev/null
         if ! check_exist unzip; then
             apt install unzip -y &>/dev/null
         fi
-        unzip ./MISC/naabu/naabu_2.1.0_linux_amd64.zip -d ./MISC/naabu/ &>/dev/null
-        mv ./MISC/naabu/naabu /usr/local/bin &>/dev/null
+        #unzip ./MISC/naabu/naabu_2.1.0_linux_amd64.zip -d ./MISC/naabu/ &>/dev/null
+        #mv ./MISC/naabu/naabu /usr/local/bin &>/dev/null
         sudo apt install libpcap0.8-dev libuv1-dev -y &>/dev/null
     fi
     if ! check_exist interlace; then
