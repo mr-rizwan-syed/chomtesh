81ac7d651eab03256667e31c0ae6c9c702087f18
Rizwan Syed <78642744+mr-rizwan-syed@users.noreply.github.com>
2023-11-06 13:10:17 +0530

    Update hostdiscovery.sh

--- core/hostdiscovery.sh
+++ core/hostdiscovery.sh
@@ -48,49 +48,34 @@ is_private_ip() {
 }
 
 hostdiscovery(){
-    trap 'echo -e "${RED}Ctrl + C detected, That's what she said${NC} "' SIGINT
+    trap 'echo -e "${RED}Ctrl + C detected, Thats what she said${NC} "' SIGINT
     echo -e ''
+    echo -e "${bluebg}1. Nmap Ping Scan Only on $1${NC}"
+    nmap -sn $1 -oG $results/hostdiscovery1.gnmap 2>&1 &>/dev/null
+    [ -e "$results/hostdiscovery1.gnmap" ] && cat $results/hostdiscovery1.gnmap | grep Host | cut -d ' ' -f 2 | anew -q $results/aliveip.txt
     
-    if [ -f "$1" ]; then
-        # Input is a file containing a list of hostnames or IP addresses
-        input_file="$1"
-    else
-        # Input is a single hostname or IP address
-        input_file="$results/hostdiscovery_input.txt"
-        echo "$1" > "$input_file"
-    fi
-
-    echo -e "${bluebg}1. Nmap Ping Scan Only${NC}"
-    nmap -sn -iL "$input_file" -oG "$results/hostdiscovery1.gnmap" 2>&1 &>/dev/null
-    [ -e "$results/hostdiscovery1.gnmap" ] && cat "$results/hostdiscovery1.gnmap" | grep Host | cut -d ' ' -f 2 | anew -q "$results/aliveip.txt"
+    echo -e "${bluebg}2. Nmap TCP SYN ping scan on $1${NC}"
+    nmap -sn -PS -n $1 -oG $results/hostdiscovery2.gnmap 2>&1 &>/dev/null
+    [ -e "$results/hostdiscovery2.gnmap" ] && cat $results/hostdiscovery2.gnmap | grep Host | cut -d ' ' -f 2 | anew -q $results/aliveip.txt
     
-    echo -e "${bluebg}2. Nmap TCP SYN ping scan${NC}"
-    nmap -sn -PS -iL "$input_file" -oG "$results/hostdiscovery2.gnmap" 2>&1 &>/dev/null
-    [ -e "$results/hostdiscovery2.gnmap" ] && cat "$results/hostdiscovery2.gnmap" | grep Host | cut -d ' ' -f 2 | anew -q "$results/aliveip.txt"
-    
-    echo -e "${bluebg}3. Nmap TCP ACK ping scan${NC}"
-    nmap -sn -PA -iL "$input_file" -oG "$results/hostdiscovery3.gnmap" 2>&1 &>/dev/null
-    [ -e "$results/hostdiscovery3.gnmap" ] && cat "$results/hostdiscovery3.gnmap" | grep Host | cut -d ' ' -f 2 | anew -q "$results/aliveip.txt"
+    echo -e "${bluebg}3. Nmap TCP ACK ping scan on $1${NC}"
+    nmap -sn -PA -n $1 -oG $results/hostdiscovery3.gnmap 2>&1 &>/dev/null
+    [ -e "$results/hostdiscovery3.gnmap" ] && cat $results/hostdiscovery3.gnmap | grep Host | cut -d ' ' -f 2 | anew -q $results/aliveip.txt
         
-    is_private_ip "$1"
-    [[ $is_private -eq 1 ]] && echo -e "${bluebg}4. Nmap ARP Ping Scan${NC}"
-    [[ $is_private -eq 1 ]] && nmap -sn -PR -iL "$input_file" -oG "$results/hostdiscovery4.gnmap" 2>&1 &>/dev/null
-    [ -e "$results/hostdiscovery4.gnmap" ] && cat "$results/hostdiscovery4.gnmap" | grep Host | cut -d ' ' -f 2 | anew -q "$results/aliveip.txt"
+    is_private_ip $1
+    [[ $is_private -eq 1 ]] && echo -e "${bluebg}4. Nmap ARP Ping Scan on $1${NC}"
+    [[ $is_private -eq 1 ]] && nmap -sn -PR -n $1 -oG $results/hostdiscovery4.gnmap 2>&1 &>/dev/null
+    [ -e "$results/hostdiscovery4.gnmap" ] && cat $results/hostdiscovery4.gnmap | grep Host | cut -d ' ' -f 2 | anew -q $results/aliveip.txt
     
-    [[ $is_private -eq 1 ]] && echo -e "${bluebg}5. Nmap ICMP Echo Request Scan${NC}"
-    [[ $is_private -eq 1 ]] && nmap -sn -PE -iL "$input_file" -oG "$results/hostdiscovery5.gnmap" 2>&1 &>/dev/null
-    [ -e "$results/hostdiscovery5.gnmap" ] && cat "$results/hostdiscovery5.gnmap" | grep Host | cut -d ' ' -f 2 | anew -q "$results/aliveip.txt"
+    [[ $is_private -eq 1 ]] && echo -e "${bluebg}5. Nmap ICMP Echo Request Scan on $1${NC}"
+    [[ $is_private -eq 1 ]] && nmap -sn -PE -n $1 -oG $results/hostdiscovery5.gnmap 2>&1 &>/dev/null
+    [ -e "$results/hostdiscovery5.gnmap" ] && cat $results/hostdiscovery5.gnmap | grep Host | cut -d ' ' -f 2 | anew -q $results/aliveip.txt
     
-    [[ $is_private -eq 1 ]] && echo -e "${bluebg}6. Nmap UDP Ping Scan${NC}"
-    [[ $is_private -eq 1 ]] && nmap -sn -PU -iL "$input_file" -oG "$results/hostdiscovery6.gnmap" 2>&1 &>/dev/null
-    [ -e "$results/hostdiscovery6.gnmap" ] && cat "$results/hostdiscovery6.gnmap" | grep Host | cut -d ' ' -f 2 | anew -q "$results/aliveip.txt"
+    [[ $is_private -eq 1 ]] && echo -e "${bluebg}6. Nmap UDP Ping Scan on $1${NC}"
+    [[ $is_private -eq 1 ]] && nmap -sn -PU -n $1 -oG $results/hostdiscovery6.gnmap 2>&1 &>/dev/null
+    [ -e "$results/hostdiscovery6.gnmap" ] && cat $results/hostdiscovery6.gnmap | grep Host | cut -d ' ' -f 2 | anew -q $results/aliveip.txt
     
-    rm "$results/hostdiscovery*.gnmap"
-    
-    # Remove the temporary input file if it was created
-    if [ -f "$1" ]; then
-        rm "$input_file"
-    fi
+    rm $results/hostdiscovery*.gnmap
 }
 
 nmapdiscovery(){
