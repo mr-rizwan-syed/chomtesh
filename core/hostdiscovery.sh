2a8e21373d9b300bf821d871f39e7815dd015e37
Rizwan Syed <78642744+mr-rizwan-syed@users.noreply.github.com>
2023-11-06 15:07:05 +0530

    Add files via upload

--- core/hostdiscovery.sh
+++ core/hostdiscovery.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-#title: CHOMTE.SH - hostdiscovery.sh V1.3
-#description:   Automated and Modular Shell Script to Automate Security Vulnerability Reconnaisance Scans
-#author:        mr-rizwan-syed
-#This script will do host discovery of CIDR / ASN 
-#==============================================================================
 
-# Function to check if the argument is a valid CIDR notation
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-YELLOW=`tput setaf 3`
-BLUE=`tput setaf 4`
-MAGENTA=`tput setaf 5`
-CYAN=`tput setaf 6`
-NC=`tput sgr0`
+# Define colors for output
+RED=$(tput setaf 1)
+BLUEBG=$(tput setab 4)
+NC=$(tput sgr0)
 
-casn=$1
-
+# Check if the argument is a valid CIDR notation
 is_cidr() {
     local cidr_pattern="^([0-9]{1,3}\.){3}[0-9]{1,3}/([0-9]|[1-2][0-9]|3[0-2])$"
     if [[ $1 =~ $cidr_pattern ]]; then
@@ -24,7 +14,6 @@ is_cidr() {
         return 1
     fi
 }
-
 # Function to check if the argument is a valid ASN (Autonomous System Number)
 is_asn() {
     local asn_pattern="^AS[0-9]+$"
@@ -35,56 +24,78 @@ is_asn() {
     fi
 }
 
+# Function to check if an IP address is private
 is_private_ip() {
-  local ip="$1"
-  local -a private_ranges=("10." "192.168." "172.16." "172.17." "172.18." "172.19." "172.20." "172.21." "172.22." "172.23." "172.24." "172.25." "172.26." "172.27." "172.28." "172.29." "172.30." "172.31.")
+    local ip="$1"
+    local -a private_ranges=("10." "192.168." "172.16." "172.17." "172.18." "172.19." "172.20." "172.21." "172.22." "172.23." "172.24." "172.25." "172.26." "172.27." "172.28." "172.29." "172.30." "172.31.")
 
-  for range in "${private_ranges[@]}"; do
-    if [[ "$ip" == $range* ]]; then
-      is_private=1
-      return
+    for range in "${private_ranges[@]}"; do
+        if [[ "$ip" == "$range"* ]]; then
+            return 1
+        fi
+    done
+
+    return 0
+}
+
+# Function for host discovery
+hostdiscovery() {
+    trap 'echo -e "${RED}Ctrl + C detected, That'\''s what she said${NC}"' SIGINT
+
+    if [ -f "$1" ]; then
+        # Input is a file containing a list of hostnames or IP addresses
+        input_file="$1"
+    else
+        # Input is a single hostname or IP address
+        input_file="/tmp/hostdiscovery_input.txt"
+        echo "$1" > "$input_file"
     fi
-  done
-}
 
-hostdiscovery(){
-    trap 'echo -e "${RED}Ctrl + C detected, Thats what she said${NC} "' SIGINT
-    echo -e ''
-    echo -e "${bluebg}1. Nmap Ping Scan Only on $1${NC}"
-    nmap -sn $1 -oG $results/hostdiscovery1.gnmap 2>&1 &>/dev/null
-    [ -e "$results/hostdiscovery1.gnmap" ] && cat $results/hostdiscovery1.gnmap | grep Host | cut -d ' ' -f 2 | anew -q $results/aliveip.txt
-    
-    echo -e "${bluebg}2. Nmap TCP SYN ping scan on $1${NC}"
-    nmap -sn -PS -n $1 -oG $results/hostdiscovery2.gnmap 2>&1 &>/dev/null
-    [ -e "$results/hostdiscovery2.gnmap" ] && cat $results/hostdiscovery2.gnmap | grep Host | cut -d ' ' -f 2 | anew -q $results/aliveip.txt
-    
-    echo -e "${bluebg}3. Nmap TCP ACK ping scan on $1${NC}"
-    nmap -sn -PA -n $1 -oG $results/hostdiscovery3.gnmap 2>&1 &>/dev/null
-    [ -e "$results/hostdiscovery3.gnmap" ] && cat $results/hostdiscovery3.gnmap | grep Host | cut -d ' ' -f 2 | anew -q $results/aliveip.txt
-        
-    is_private_ip $1
-    [[ $is_private -eq 1 ]] && echo -e "${bluebg}4. Nmap ARP Ping Scan on $1${NC}"
-    [[ $is_private -eq 1 ]] && nmap -sn -PR -n $1 -oG $results/hostdiscovery4.gnmap 2>&1 &>/dev/null
-    [ -e "$results/hostdiscovery4.gnmap" ] && cat $results/hostdiscovery4.gnmap | grep Host | cut -d ' ' -f 2 | anew -q $results/aliveip.txt
-    
-    [[ $is_private -eq 1 ]] && echo -e "${bluebg}5. Nmap ICMP Echo Request Scan on $1${NC}"
-    [[ $is_private -eq 1 ]] && nmap -sn -PE -n $1 -oG $results/hostdiscovery5.gnmap 2>&1 &>/dev/null
-    [ -e "$results/hostdiscovery5.gnmap" ] && cat $results/hostdiscovery5.gnmap | grep Host | cut -d ' ' -f 2 | anew -q $results/aliveip.txt
-    
-    [[ $is_private -eq 1 ]] && echo -e "${bluebg}6. Nmap UDP Ping Scan on $1${NC}"
-    [[ $is_private -eq 1 ]] && nmap -sn -PU -n $1 -oG $results/hostdiscovery6.gnmap 2>&1 &>/dev/null
-    [ -e "$results/hostdiscovery6.gnmap" ] && cat $results/hostdiscovery6.gnmap | grep Host | cut -d ' ' -f 2 | anew -q $results/aliveip.txt
-    
-    rm $results/hostdiscovery*.gnmap
-}
+    echo -e "${BLUEBG}1. Nmap Ping Scan Only $1 ${NC}"
+    nmap -sn -iL "$input_file" -oG "$results/hostdiscovery1.gnmap" &>/dev/null
+    [ -e "$results/hostdiscovery1.gnmap" ] && cat "$results/hostdiscovery1.gnmap" | grep Host | cut -d ' ' -f 2 | anew -q "$results/aliveip.txt"
 
+    echo -e "${BLUEBG}2. Nmap TCP SYN ping scan $1 ${NC}"
+    nmap -sn -PS -iL "$input_file" -oG "$results/hostdiscovery2.gnmap" &>/dev/null
+    [ -e "$results/hostdiscovery2.gnmap" ] && cat "$results/hostdiscovery2.gnmap" | grep Host | cut -d ' ' -f 2 | anew -q "$results/aliveip.txt"
+
+    echo -e "${BLUEBG}3. Nmap TCP ACK ping scan $1 ${NC}"
+    nmap -sn -PA -iL "$input_file" -oG "$results/hostdiscovery3.gnmap" &>/dev/null
+    [ -e "$results/hostdiscovery3.gnmap" ] && cat "$results/hostdiscovery3.gnmap" | grep Host | cut -d ' ' -f 2 | anew -q "$results/aliveip.txt"
+
+    is_private_ip "$(cat "$input_file")"
+    
+    if [ "$is_private" -eq 1 ]; then
+        echo -e "${BLUEBG}4. Nmap ARP Ping Scan $1 ${NC}"
+        nmap -sn -PR -iL "$input_file" -oG "$results/hostdiscovery4.gnmap" &>/dev/null
+        [ -e "$results/hostdiscovery4.gnmap" ] && cat "$results/hostdiscovery4.gnmap" | grep Host | cut -d ' ' -f 2 | anew -q "$results/aliveip.txt"
+
+        echo -e "${BLUEBG}5. Nmap ICMP Echo Request Scan $1 ${NC}"
+        nmap -sn -PE -iL "$input_file" -oG "$results/hostdiscovery5.gnmap" &>/dev/null
+        [ -e "$results/hostdiscovery5.gnmap" ] && cat "$results/hostdiscovery5.gnmap" | grep Host | cut -d ' ' -f 2 | anew -q "$results/aliveip.txt"
+
+        echo -e "${BLUEBG}6. Nmap UDP Ping Scan $1 ${NC}"
+        nmap -sn -PU -iL "$input_file" -oG "$results/hostdiscovery6.gnmap" &>/dev/null
+        [ -e "$results/hostdiscovery6.gnmap" ] && cat "$results/hostdiscovery6.gnmap" | grep Host | cut -d ' ' -f 2 | anew -q "$results/aliveip.txt"
+    fi
+
+    rm "$results/hostdiscovery*.gnmap"
+
+    # Remove the temporary input file if it was created
+    if [ -f "$1" ]; then
+        cp $1 $results
+    fi
+}
 nmapdiscovery(){
     # Main script
     if [ $# -ne 1 ]; then
         echo "hostdiscovery.sh: $0 <argument>"
         exit 1
     fi
-    # Check if it's a valid CIDR
+
+    [[ -f "$casn" ]] && echo "CIDR File" && hostdiscovery $casn
+ 
+   # Check if it's a valid CIDR
     if is_cidr "$casn"; then
         echo "CIDR notation: $casn"
         [[ ! -e $results/aliveip.txt || $rerun == true ]] && hostdiscovery $casn
