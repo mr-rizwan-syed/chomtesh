3cc562b0173d30980abe5db61c20fdc9dc80f46a
Rizwan Syed <78642744+mr-rizwan-syed@users.noreply.github.com>
2023-11-10 14:48:04 +0530

    Update nmapScanner.sh

--- core/nmapScanner.sh
+++ core/nmapScanner.sh
@@ -6,13 +6,8 @@
 # nmapscanner <ipport-input> <nmapscans-directory>
 # nmapscanner ipport.txt nmapscans
 
-ipportin=$1
-nmapscans=$2
-
-[ ! -e $aliveip ] && cat $ipportin | cut -d : -f 1 | sort -u | grep -v ip | anew -q $aliveip
-
 scanner(){
-  ports=$(cat $ipport| grep $iphost | cut -d ':' -f 2 | sort -u | xargs | sed -e 's/ /,/g')
+  ports=$(cat $ipportin| grep $iphost | cut -d ':' -f 2 | sort -u | xargs | sed -e 's/ /,/g')
     if [ -z "$ports" ]
     then
         echo -e "No Ports found for $iphost"
@@ -31,11 +26,11 @@ nmapconverter(){
   rm $nmapscans/*.html &>/dev/null
   rm $nmapscans/*.csv &>/dev/null
   # Convert to csv
-  ls $nmapscans/*.xml | xargs -I {} python3 $PWD/MISC/xml2csv.py -f {} -csv {}.csv &>/dev/null 
-  echo -e "${GREEN}[+] All Nmap CSV Generated ${NC}"   
+  ls $nmapscans/*.xml | xargs -I {} python3 $PWD/MISC/xml2csv.py -f {} -csv {}.csv &>/dev/null
+  echo -e "${GREEN}[+] All Nmap CSV Generated ${NC}"
   # Merge all csv
   [ ! -e $nmapscans/Nmap_Final_Merged.csv ] && csvstack $nmapscans/*.csv > $nmapscans/Nmap_Final_Merged.csv 2>/dev/null
-  echo -e "${GREEN}[+] Merged Nmap CSV Generated ${NC}$nmapscans/Nmap_Final_Merged.csv"   
+  echo -e "${GREEN}[+] Merged Nmap CSV Generated ${NC}$nmapscans/Nmap_Final_Merged.csv"
   # Generating HTML Report Format
   ls $nmapscans/*.xml | xargs -I {} xsltproc -o {}_nmap.html ./MISC/nmap-bootstrap.xsl {} 2>$nmapscans/error.log
   echo -e "${GREEN}[+] HTML Report Format Generated ${NC}"
@@ -45,9 +40,14 @@ nmapconverter(){
 }
 
 nmapscanner(){
+    # Main Function
+    ipportin=$1
+    nmapscans=$2
+    [ ! -e $aliveip ] && echo -e "cat $ipportin | cut -d : -f 1 | sort -u | grep -v ip | anew -q $aliveip"
+    [ ! -e $aliveip ] && cat $ipportin | cut -d : -f 1 | sort -u | grep -v ip | anew -q $aliveip
+    cat $ipportin | cut -d : -f 1 | sort -u | while read ip; do echo -n "$ip ";grep "^$ip" $ipportin | cut -d : -f 2 | tr '\n' ' ' | echo -e Total Ports Count [$(wc -w)]; done
     mkdir -p $nmapscans
     echo -e ${YELLOW}"[*] Running Nmap Scan"${NC}
-    echo -e ${YELLOW}"[*] nmapscanner $nmapscans $ipportin "${NC}
     counter=0
     while read iphost; do
         scanner
